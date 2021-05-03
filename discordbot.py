6c036d71793b711d5b84f99eb643ee774478903c
tetuarian <61371082+tetuarian@users.noreply.github.com>
2021-05-03 23:59:56 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,18 @@
-from discord.ext import commands
-import os
-import traceback
+import discord
 
-bot = commands.Bot(command_prefix='!')
-token = os.environ['DISCORD_BOT_TOKEN']
+TOKEN = os.environ['DISCORD_BOT_TOKEN']
 
+client = discord.Client()
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+@client.event
+async def on_ready():
+    print('ログインしました')
 
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
 
-@bot.command()
-async def hoisiri(ctx):
-    await ctx.send('dai(はぁと)')
-
-
-bot.run(token)
+client.run(TOKEN)
