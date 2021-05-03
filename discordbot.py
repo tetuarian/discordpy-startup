b0c634b2f40c0acbdfd03c3218567c736be7b8d4
tetuarian <61371082+tetuarian@users.noreply.github.com>
2021-05-04 03:58:37 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 bot = commands.Bot(command_prefix='!')
 token = os.environ['DISCORD_BOT_TOKEN']
+CHANNEL_ID = 829494676477181955
 
 
 @bot.event
@@ -16,7 +17,14 @@ async def on_command_error(ctx, error):
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('pong')
+    await ctx.send('pong'):
+async def loop():
+    now = datetime.now().strftime('%H:%M')
+    if now == '04:00':
+        channel = client.get_channel(CHANNEL_ID)
+        await channel.send('おはよう')  
+
+loop.start()
 
 
 bot.run(token)
